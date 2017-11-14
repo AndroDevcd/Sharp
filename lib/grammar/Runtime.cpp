004a491c2ae26c29e31b8de9977abd5e75a090aa
AndroDevcd <braxtonnunnally@gmail.com>
2017-11-13 20:00:38 -0600

    [LMAPL] episode 1

---
+++ lib/grammar/Runtime.cpp
@@ -0,0 +1,36 @@
+//
+// Created by bknun on 9/12/2017.
+//
+
+#include "Runtime.h"
+
+void help();
+
+int _bootstrap(int argc, const char* argv[])
+{
+    if(argc < 2) {
+        help();
+        return 1;
+    }
+    
+}
+
+void help() {
+    cout << "Usage: bootstrap" << "{OPTIONS} SOURCE FILE(S)" << std::endl;
+    cout << "Source file must have a .sharp extion to be compiled.\n" << endl;
+    cout << "[-options]\n\n    -V                print compiler version and exit"                   << endl;
+    cout <<               "    -showversion      print compiler version and continue"               << endl;
+    cout <<               "    -o<file>          set the output object file"                        << endl;
+    cout <<               "    -c                compile only and do not generate exe"              << endl;
+    cout <<               "    -a                enable aggressive error reporting"                 << endl;
+    cout <<               "    -s                string debugging info"                             << endl;
+    cout <<               "    -O                optimize executable"                               << endl;
+    cout <<               "    -w                disable all warnings"                              << endl;
+    cout <<               "    -v<version>       set the application version"                       << endl;
+    cout <<               "    -unsafe -u        allow unsafe code"                                 << endl;
+    cout <<               "    -objdmp           create dump file for generated assembly"           << endl;
+    cout <<               "    -target           target the specified platform of sharp to run on"  << endl;
+    cout <<               "    -werror           enable warnings as errors"                         << endl;
+    cout <<               "    -release -r       generate a release build exe"                      << endl;
+    cout <<               "    --h -?            display this help message"                         << endl;
+}
---
+++ lib/grammar/Runtime.h
@@ -0,0 +1,16 @@
+//
+// Created by bknun on 9/12/2017.
+//
+
+#ifndef SHARP_RUNTIME_H
+#define SHARP_RUNTIME_H
+
+#include "../../stdimports.h"
+
+class Runtime {
+
+};
+
+int _bootstrap(int argc, const char* argv[]);
+
+#endif //SHARP_RUNTIME_H
