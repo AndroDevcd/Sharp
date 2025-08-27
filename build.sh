00433c3c0f5e92a36c391df5010c7ae9fbd008e1
Dev {Code} <braxtonnunnally@Braxtons-MacBook-Pro-2.local>
2025-08-27 01:33:23 -0400

    updated build file

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Detect OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS-specific flags (no -lrt and no --no-as-needed)
+    CXX_COMPILER="g++-15"
+    PLATFORM_FLAGS=""
+    INSTALL_BIN_DIR="/usr/local/bin"
+    INSTALL_INCLUDE_DIR="/usr/local/include/sharp"
+else
+    # default flags
+    CXX_COMPILER="g++"
+    PLATFORM_FLAGS="-Wl,--no-as-needed -lrt -mno-red-zone"
+    INSTALL_BIN_DIR="/bin"
+    INSTALL_INCLUDE_DIR="/usr/include/sharp"
+fi
+
 # Function to build the Sharp compiler
 build_compiler() {
     echo "Building Sharp compiler..."
-    g++ -o bin/sharpc -std=c++11 -pthread -lstdc++ -O3 -Ofast -g main.cpp stdimports.h -DMAKE_COMPILER \
+    $CXX_COMPILER -o bin/sharpc -std=c++11 -pthread -lstdc++ -O3 -Ofast -g main.cpp stdimports.h -DMAKE_COMPILER \
         lib/grammar/*.h lib/grammar/*.cpp \
         lib/grammar/json/*.h lib/grammar/json/*.cpp \
         lib/grammar/settings/*.h lib/grammar/settings/*.cpp \
@@ -39,7 +54,7 @@ build_compiler() {
         lib/util/*.cpp lib/util/zip/*.cpp \
         lib/core/*.cpp lib/core/*.h \
         lib/core/opcode/*.cpp lib/core/opcode/*.h
-    
+
     if [ $? -eq 0 ]; then
         echo "Sharp compiler built successfully!"
     else
@@ -51,7 +66,7 @@ build_compiler() {
 # Function to build the Sharp runtime
 build_runtime() {
     echo "Building Sharp runtime..."
-    g++ -o bin/sharp -std=c++11 -pthread -lstdc++ -Wl,--no-as-needed -lm -lrt -lstdc++ -O3 -ldl -mno-red-zone -g -Ofast main.cpp stdimports.h \
+    $CXX_COMPILER -o bin/sharp -std=c++17 -pthread $PLATFORM_FLAGS -DASMJIT_STATIC -lm -lstdc++ -O3 -ldl -g -Ofast main.cpp stdimports.h \
         lib/runtime/error/*.h lib/runtime/error/*.cpp \
         lib/runtime/exe/*.h lib/runtime/exe/*.cpp \
         lib/runtime/memory/*.h lib/runtime/memory/*.cpp \
@@ -59,6 +74,10 @@ build_runtime() {
         lib/runtime/snb/*.h lib/runtime/snb/*.cpp \
         lib/runtime/multitasking/*/*.h lib/runtime/multitasking/*/*.cpp \
         lib/runtime/types/*.h lib/runtime/types/*.cpp \
+        lib/runtime/jit/*.h lib/runtime/jit/*.cpp \
+        lib/runtime/jit/arm/*.h lib/runtime/jit/arm/*.cpp \
+        lib/runtime/jit/arm/opcode/*.cpp \
+        lib/runtime/jit/arm/sections/*.cpp \
         lib/runtime/*.h lib/runtime/*.cpp \
         lib/core/*.cpp lib/core/*.h \
         lib/core/opcode/*.cpp lib/core/opcode/*.h \
@@ -66,8 +85,15 @@ build_runtime() {
         lib/util/zip/*.cpp lib/util/zip/*.h \
         lib/Modules/math/*.h lib/Modules/math/*.cpp \
         lib/Modules/std/*.h lib/Modules/std/*.cpp \
-        lib/Modules/std.io/*.h lib/Modules/std.io/*.cpp
-    
+        lib/Modules/std.io/*.h lib/Modules/std.io/*.cpp \
+        lib/util/jit/asmjit/src/asmjit/core/*.h \
+        lib/util/jit/asmjit/src/asmjit/arm/*.h \
+        lib/util/jit/asmjit/src/asmjit/x86/*.cpp \
+        lib/util/jit/asmjit/src/asmjit/x86/*.h \
+        lib/util/jit/asmjit/src/asmjit/core/*.cpp \
+        lib/util/jit/asmjit/src/asmjit/arm/*.cpp \
+        lib/util/jit/asmjit/src/asmjit/*.h
+
     if [ $? -eq 0 ]; then
         echo "Sharp runtime built successfully!"
     else
@@ -79,24 +105,24 @@ build_runtime() {
 # Function to copy files and install standard library
 install_files() {
     echo "Installing Sharp files..."
-    
+
     # Copy binaries if they exist
     if [ -f "bin/sharpc" ]; then
-        sudo cp bin/sharpc /bin
-        echo "Sharp compiler installed to /bin/sharpc"
+        sudo cp bin/sharpc $INSTALL_BIN_DIR
+        echo "Sharp compiler installed to $INSTALL_BIN_DIR/sharpc"
     fi
-    
+
     if [ -f "bin/sharp" ]; then
-        sudo cp bin/sharp /bin
-        echo "Sharp runtime installed to /bin/sharp"
+        sudo cp bin/sharp $INSTALL_BIN_DIR
+        echo "Sharp runtime installed to $INSTALL_BIN_DIR/sharp"
     fi
-    
+
     # Install standard library
     echo "Installing standard library..."
-    sudo rm -rf /usr/include/sharp/
-    sudo mkdir -p /usr/include/sharp/
-    sudo cp -r lib/support/0.3.0/ /usr/include/sharp/
-    
+    sudo rm -rf $INSTALL_INCLUDE_DIR
+    sudo mkdir -p $INSTALL_INCLUDE_DIR
+    sudo cp -r lib/support/0.3.0/ $INSTALL_INCLUDE_DIR
+
     echo "Sharp installed!"
 }
 
